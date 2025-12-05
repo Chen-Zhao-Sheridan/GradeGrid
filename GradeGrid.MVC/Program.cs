3d508b78b72421cc9f79b32b7072e3a3a816785b
Rishabh Mishra <mishrarishabh075@gmail.com>
2025-12-05 12:22:03 -0500

    Registered an HttpClient

--- GradeGrid.MVC/Program.cs
+++ GradeGrid.MVC/Program.cs
@@ -1,15 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient("GradeGridApi", client =>
+{
+    var baseUrl = builder.Configuration["ApiBaseUrl"] ?? "https://localhost:5000/";
+    client.BaseAddress = new Uri(baseUrl);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseRouting();
 
 app.UseAuthorization();
@@ -20,6 +25,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
